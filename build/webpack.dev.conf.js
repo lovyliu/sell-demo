10ae2f1b03b612ea791bc01687790669e831eb69
lovy <lyao1031@gmail.com>
2018-03-23 15:17:33 +0800

    update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -13,6 +13,11 @@ const portfinder = require('portfinder')
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
+const jsonData = require('../data.json')
+const seller = jsonData.seller
+const goods = jsonData.goods
+const ratings = jsonData.ratings
+
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
@@ -22,6 +27,26 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    before(app) {
+      app.get('/api/seller', function(req, res) {
+        res.json({
+          errorno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function(req, res) {
+        res.json({
+          errorno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function(req, res) {
+        res.json({
+          errorno: 0,
+          data: ratings
+        })
+      })
+    },
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header :seller="seller">
+
+    </Header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link :to="{ name: 'goods', params: {} }">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{ name: 'ratings', params: {} }">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{ name: 'seller', params: {} }">商家</router-link>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/header/header';
+const ERR_OK = 0;
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+  data () {
+    return {
+      seller: {}
+    };
+  },
+  created () {
+    this.$http.get('/api/seller').then((response) => {
+      response = response.body;
+      if (response.errorno === ERR_OK) {
+        this.seller = response.data;
+        console.log(this.seller);
+      }
+    });
+  },
+  components: {
+    Header: Header
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display:flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77,85,93)
+        &.active
+          color: rgb(240, 20, 20)
 </style>
